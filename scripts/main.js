cc0502c12f605bb10941b6ea41757eaa24fb45c3
Ahmed Salah <67317329+a7medsalah169@users.noreply.github.com>
2024-10-20 18:47:29 +0300

    add style to auth page
    make style to sign in and up page

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+// function goToLogin(){
+// document.getElementById('Login').addEventListener('click', function() {
+//     window.location.href = 'login.html';});
+// };
+// function goToSinup(){
+// document.getElementById('signup').addEventListener('click', function() {
+//     window.location.href = 'signup.html';});
+// };
+
