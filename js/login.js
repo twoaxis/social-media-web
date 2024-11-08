4e060c43cccc12adb1d5e4f2736fe10e6270809c
Ahmed Salah <67317329+a7medsalah169@users.noreply.github.com>
2024-11-08 16:57:04 +0200

    add auth.js
    don't allow to any one go to main pages

--- js/login.js
+++ js/login.js
@@ -1,4 +1,5 @@
-// signin.js
+// login.js
+
 document.getElementById('signinForm').addEventListener('submit', function(event) {
     event.preventDefault();
     const email = document.querySelector('#signinForm input[name="email"]').value;
@@ -6,22 +7,21 @@ document.getElementById('signinForm').addEventListener('submit', function(event)
     const errorMessage = document.getElementById('error-message');
     errorMessage.style.display = 'none';
     errorMessage.innerHTML = '';
-    // POST
-    axios.post('http://18.193.81.175/auth/login', 
-        { 
-            email : email
-            , password: password 
-        })
+
+    // POST request to authenticate the user
+    axios.post('http://18.193.81.175/auth/login', { email, password })
         .then(response => {
+            const token = response.data.token;
+            localStorage.setItem('token', token);
             console.log('Login Success:', response.data);
-            window.location.href = "nav.htm";
+            window.location.href = "nav.htm"; // Redirect to home page after login
         })
         .catch(error => {
             console.error('Login Error:', error);
             errorMessage.style.display = 'block';
-            if (error.response.status === 400) {
+            if (error.response && error.response.status === 400) {
                 errorMessage.innerHTML = '<strong>Missing or incorrect fields!</strong> Please check your inputs.';
-            } else if (error.response.status === 401) {
+            } else if (error.response && error.response.status === 401) {
                 errorMessage.innerHTML = '<strong>Invalid email or password!</strong> Please try again.';
             } else {
                 errorMessage.innerHTML = '<strong>Login failed!</strong> Please try again later.';
